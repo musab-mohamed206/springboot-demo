f3afe49b2f8d3108efd558eea8d14ad2b7baef57
NINJA <musab.mohamed206@gmail.com>
2020-12-02 14:35:58 +0300

    add topic controller

---
+++ src/main/java/io/javabrains/spreingboot/courseapi/topic/Topic.java
@@ -0,0 +1,45 @@
+package io.javabrains.spreingboot.courseapi.topic;
+
+public class Topic {
+
+    private String id;
+    private String name;
+    private String description;
+
+    
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Topic() {
+
+    }
+
+    public Topic(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+    
+}
